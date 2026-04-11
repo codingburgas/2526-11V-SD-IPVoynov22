bdbed5bc6ee57f9b639faf122ea8727177238ffd
IPVoynov22 <IPVoynov22@codingburgas.bg>
2026-04-11 18:39:55 +0300

    Setup ASP.NET Core MVC project structure

---
+++ JobBoardPlatform/JobBoardPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JobBoardPlatform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
