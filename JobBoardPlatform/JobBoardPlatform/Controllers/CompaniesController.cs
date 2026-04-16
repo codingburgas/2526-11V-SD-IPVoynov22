07085519e0d792cc4e54d843cb13ac747e23a730
IPVoynov22 <IPVoynov22@codingburgas.bg>
2026-04-16 14:53:55 +0300

    Implement Companies controller and views

---
+++ JobBoardPlatform/JobBoardPlatform/Controllers/CompaniesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JobBoardPlatform.Data;
+using JobBoardPlatform.Models;
+
+namespace JobBoardPlatform.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompaniesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Companies
+        public async Task<IActionResult> Index()
+        {
+            var companies = await _context.Companies.ToListAsync();
+            return View(companies);
+        }
+
+        // GET: Companies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(company);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+    }
+}
--- JobBoardPlatform/JobBoardPlatform/Models/Company.cs
+++ JobBoardPlatform/JobBoardPlatform/Models/Company.cs
@@ -6,9 +6,9 @@ public class Company : BaseEntity
 {
     [Required]
     [StringLength(100)]
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     // Navigation property: One Company can have Many JobPostings (1-to-Many relationship)
     public virtual ICollection<JobPosting> JobPostings { get; set; } = new List<JobPosting>();
